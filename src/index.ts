ddd69d65b94ca60b25b641fa3d3a170852813273
aswinsuriya16 <aswinsuriya16@gmail.com>
2025-07-31 21:10:11 +0000

    CRUD

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,44 @@
-console.log("Hi there")
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
+
+async function main() {
+  const createdUser = await client.user.create({
+    data: {
+      username: "ashh",
+      password: "21312",
+      City: "TVL",
+    },
+  });
+  console.log("User created:", createdUser);
+
+  await client.user.delete({
+    where: {
+      id: createdUser.id,
+    },
+  });
+  console.log("user deleted");
+
+  const recreatedUser = await client.user.create({
+    data: {
+      username: "aswin-old",
+      password: "12345",
+      City: "TVL",
+    },
+  });
+
+  const updatedUser = await client.user.update({
+    where: {
+      id: recreatedUser.id,
+    },
+    data: {
+      username: "aswin",
+    },
+  });
+  console.log("user updated:", updatedUser);
+
+  const user = await client.user.findFirst();
+  console.log("First user:", user);
+}
+
+main();
