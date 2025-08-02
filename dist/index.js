719bd81839d06ab6d900543d97bdb515a7e4fd91
aswinsuriya16 <aswinsuriya16@gmail.com>
2025-08-02 09:27:03 +0000

    relationship

--- dist/index.js
+++ dist/index.js
@@ -11,40 +11,67 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 const client_1 = require("@prisma/client");
 const client = new client_1.PrismaClient();
+// async function main() {
+//   const createdUser = await client.user.create({
+//     data: {
+//       username: "ashh",
+//       password: "21312",
+//       City: "TVL",
+//     },
+//   });
+//   console.log("User created:", createdUser);
+//   await client.user.delete({
+//     where: {
+//       id: createdUser.id,
+//     },
+//   });
+//   console.log("user deleted");
+//   const recreatedUser = await client.user.create({
+//     data: {
+//       username: "aswin-old",
+//       password: "12345",
+//       City: "TVL",
+//     },
+//   });
+//   const updatedUser = await client.user.update({
+//     where: {
+//       id: recreatedUser.id,
+//     },
+//     data: {
+//       username: "aswin",
+//     },
+//   });
+//   console.log("user updated:", updatedUser);
+//   const user = await client.user.findFirst();
+//   console.log("First user:", user);
+// }
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
-        const createdUser = yield client.user.create({
-            data: {
-                username: "ashh",
-                password: "21312",
-                City: "TVL",
-            },
-        });
-        console.log("User created:", createdUser);
-        yield client.user.delete({
+        // const createUser = await client.user.create({
+        //     data : {
+        //         username : "aditya",
+        //         password : "erode",
+        //         City : "Erode"
+        //     }
+        // })
+        // console.log(createUser);
+        // const addTodo = await client.todo.create({
+        //     data : {
+        //         title : "shot",
+        //         done : true,
+        //         userId : createUser.id
+        //     }
+        // })
+        const id1 = yield client.user.findFirst({
             where: {
-                id: createdUser.id,
+                id: 6
             },
+            include: {
+                todos: true
+            }
         });
-        console.log("user deleted");
-        const recreatedUser = yield client.user.create({
-            data: {
-                username: "aswin-old",
-                password: "12345",
-                City: "TVL",
-            },
-        });
-        const updatedUser = yield client.user.update({
-            where: {
-                id: recreatedUser.id,
-            },
-            data: {
-                username: "aswin",
-            },
-        });
-        console.log("user updated:", updatedUser);
-        const user = yield client.user.findFirst();
-        console.log("First user:", user);
+        console.log(id1);
+        // console.log(addTodo);
     });
 }
 main();
--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,74 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 const client = new PrismaClient();
 
+// async function main() {
+//   const createdUser = await client.user.create({
+//     data: {
+//       username: "ashh",
+//       password: "21312",
+//       City: "TVL",
+//     },
+//   });
+//   console.log("User created:", createdUser);
+
+//   await client.user.delete({
+//     where: {
+//       id: createdUser.id,
+//     },
+//   });
+//   console.log("user deleted");
+
+//   const recreatedUser = await client.user.create({
+//     data: {
+//       username: "aswin-old",
+//       password: "12345",
+//       City: "TVL",
+//     },
+//   });
+
+//   const updatedUser = await client.user.update({
+//     where: {
+//       id: recreatedUser.id,
+//     },
+//     data: {
+//       username: "aswin",
+//     },
+//   });
+//   console.log("user updated:", updatedUser);
+
+//   const user = await client.user.findFirst();
+//   console.log("First user:", user);
+// }
+
 async function main() {
-  const createdUser = await client.user.create({
-    data: {
-      username: "ashh",
-      password: "21312",
-      City: "TVL",
-    },
-  });
-  console.log("User created:", createdUser);
+    const createUser = await client.user.create({
+        data : {
+            username : "aditya",
+            password : "erode",
+            City : "Erode"
+        }
+    })
+    console.log(createUser);
 
-  await client.user.delete({
-    where: {
-      id: createdUser.id,
-    },
-  });
-  console.log("user deleted");
+    const addTodo = await client.todo.create({
+        data : {
+            title : "shot",
+            done : true,
+            userId : createUser.id
+        }
+    })
 
-  const recreatedUser = await client.user.create({
-    data: {
-      username: "aswin-old",
-      password: "12345",
-      City: "TVL",
-    },
-  });
-
-  const updatedUser = await client.user.update({
-    where: {
-      id: recreatedUser.id,
-    },
-    data: {
-      username: "aswin",
-    },
-  });
-  console.log("user updated:", updatedUser);
-
-  const user = await client.user.findFirst();
-  console.log("First user:", user);
+    const id1 = await client.user.findFirst({
+        where : {
+            id : 1
+        },
+        include : {
+            todos : true
+        }
+    })
+    console.log(id1);
+    console.log(addTodo);
 }
 
 main();
